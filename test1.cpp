fd434177e5506e5e0cbcfdf09389f268eb038d1d
Toushif <toushif.haq@gmail.com>
2022-03-16 04:52:54 +0530

    test1 practice array

--- test1.cpp
+++ test1.cpp
@@ -4,11 +4,20 @@ using namespace std;
 int main() {
     string first_name, last_name;
 
-    cout << "Enter your first name-";
-    cin >> first_name;
-    cout << "Enter your last name-";
-    cin >> last_name;
+    // cout << "Enter your first name-";
+    // cin >> first_name;
+    // cout << "Enter your last name-";
+    // cin >> last_name;
 
-    cout << "Your full name is " << first_name + " " + last_name;
+    // cout << "Your full name is " << first_name + " " + last_name;
+
+    int A[5];
+    A[1] = 12;
+    A[2] = 29;
+
+    cout << sizeof(A) << endl;
+    cout << "A[0] is " << A[0] << endl;
+    cout << "A[2] is " << A[2] << endl;
+
     return 0;
 }
