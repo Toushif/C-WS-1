9d4f39ee07f0fe0c48b6dc4f98cbabd0d5c5f65c
Toushif <toushif.haq@gmail.com>
2022-03-16 05:01:47 +0530

    test1 practice array

--- test1.cpp
+++ test1.cpp
@@ -15,10 +15,13 @@ int main() {
     A[1] = 12;
     A[2] = 29;
 
+
     cout << sizeof(A) << endl; //sizeof is used to determine the space the array has consumed in memeory, so above int A[5] has 5 integers, so each integer will occupy 4 bytes, so total size -> 4*5 = 20 bytes.
     cout << "A[0] is " << A[0] << endl;
     cout << "A[2] is " << A[2] << endl;
     printf("%d\n", A[1]); //C code will also work in a C++ code
+
+    int B[10] = {2,7,4,8,6}; // Here array B is initialized as an array of size 10 but only has 5 values, so the rest of the values from index 5 to 9 will be 0 by default. If an array is not initiaized at all then by default the values will be random or garbage values.
 
     return 0;
 }
