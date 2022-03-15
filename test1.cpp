c850f78ca3097f20a7a6337eb90705ccdf4d0bde
Toushif <toushif.haq@gmail.com>
2022-03-16 04:57:03 +0530

    test1 practice array

--- test1.cpp
+++ test1.cpp
@@ -15,7 +15,7 @@ int main() {
     A[1] = 12;
     A[2] = 29;
 
-    cout << sizeof(A) << endl;
+    cout << sizeof(A) << endl; //sizeof is used to determine the space the array has consumed in memeory, so above int A[5] has 5 integers, so each integer will occupy 4 bytes, so total size -> 4*5 = 20 bytes.
     cout << "A[0] is " << A[0] << endl;
     cout << "A[2] is " << A[2] << endl;
     printf("%d\n", A[1]); //C code will also work in a C++ code
