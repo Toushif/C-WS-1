eed77d23766a818178d00c05eb8db80e1a7288d8
Toushif <toushif.haq@gmail.com>
2022-03-17 04:57:12 +0530

    cpp reference edited

--- reference.cpp
+++ reference.cpp
@@ -1,7 +1,14 @@
-// Reference in cpp is an alias name to other variables
+// Reference in cpp is an alias name to other variables. It is useful for parameter passing in functions.
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    
+    int a = 10;
+    int &r =  a; // r is a reference and is indicated by '&' in front. The address of a and r is the SAME. Both the variables are ponting to the save address in memory, therefore same value.
+    cout << a << endl;
+    r++;
+    cout << r;
+
+    return 0;
 }
