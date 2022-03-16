e9cac69e4c4bb8008b8c5333405837a2ed95a2c3
Toushif <toushif.haq@gmail.com>
2022-03-17 03:20:13 +0530

    cpp poniters edited

--- pointers.cpp
+++ pointers.cpp
@@ -1,3 +1,10 @@
 // Pointer is an address variable that is meant for storing address of data and not data itself. Pointers are used for indirectly accessing the data. 
 // Pointers are used for accessing Heap memory. It is used to access all the resources that are external to the program - that is a hard disk file or keyboard, monitor, internet, etc - anythig exterenal to program.
 // It is also used for passing the parameters.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+}
