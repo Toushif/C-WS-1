318fe898d2b325be8bb2d59c6b609f5c4725cf5c
Toushif <toushif.haq@gmail.com>
2022-03-17 05:20:31 +0530

    cpp pointerToStructure edited

--- pointerToStructure.cpp
+++ pointerToStructure.cpp
@@ -11,7 +11,8 @@ int main() {
     struct Rectangle *p = &r; // this pointer will point to the address of variable 'r'. Here we are statically creating a pointer of size 4 (since all pointers are of same size unless allocated manually), but the Rectangle as we know is of size of 8 (4+4).
     r.length = 20; // modifying the length of r
     (*p).length = 20; // Here we are modifying the length of r using pointer. This is the syntax for de-referencing and assigning for pointer to structure
-    p->length = 20; // This is the shortcut syntax to the above same operation.
+    p->length = 22; // This is the shortcut syntax to the above same operation.
+    cout << r.length << endl; // 22
 
     return 0;
 }
