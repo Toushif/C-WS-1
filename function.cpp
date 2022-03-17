504a8dc7eaa3e27300a9bc582734b4064c7ba3ff
Toushif <toushif.haq@gmail.com>
2022-03-18 02:03:45 +0530

    cpp function edted

--- function.cpp
+++ function.cpp
@@ -18,8 +18,14 @@ void swap2(int &a, int &b) {
     b = temp;
 };
 
-void fun1(int A[]) { // When you pass an array in a function, then the array is passed as a pointer pointing to the first elements's address in memory, so the size of array info is lost.
+void fun1(int A[]) { // When you pass an array in a function, then the array is passed as a pointer pointing to the first elements's address in memory, so the size of array info is lost. 
     cout << sizeof(A)/sizeof(int) << endl;
+}
+
+void fun2(int A[], int n) { // You also cannot use for each loop inside this function on 'A' coz A is a pointer and not an array. You have to use traditional for loop.
+    for(int i = 0; i < n; i++) {
+        cout << A[i] << endl;
+    }
 }
 
 int main() {
@@ -40,6 +46,7 @@ int main() {
     int A1[] = {2,4,6,8,10};
     int n = 5;
     fun1(A1);
+    fun2(A1, n);
 
     cout << sizeof(A1)/sizeof(int) << endl;
 
