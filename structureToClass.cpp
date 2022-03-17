92883e99f6853315a0cff0918ebf67e54395a807
Toushif <toushif.haq@gmail.com>
2022-03-18 04:03:42 +0530

    cpp structure to class based style of coding edited

--- structureToClass.cpp
+++ structureToClass.cpp
@@ -1,5 +1,5 @@
 // Program example in C lang style of coding using structures-
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 struct Rectangle {
@@ -28,6 +28,38 @@ int main() {
     changeLength(&r, 12);
 
     cout << "Area is " << a << endl << "Length is " << r.length << endl;
+
+    return 0;
+} */
+
+// Same program example using C++ object oriented style of programming using classes -
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    private: int length;
+    private: int breadth;
+
+    public: Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    };
+
+    public: int area() {
+        return length * breadth;
+    };
+
+    public: void changeLength(int l) {
+        length = l;
+    };
+};
+
+int main() {
+    Rectangle r(10, 6);
+    int a  = r.area();
+    r.changeLength(12);
+
+    cout << "Area is " << a << endl;
 
     return 0;
 }
