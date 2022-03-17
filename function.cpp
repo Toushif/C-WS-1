545ecc6d4496187c1a2391bb9dba89f5658d158b
Toushif <toushif.haq@gmail.com>
2022-03-17 06:36:42 +0530

    cpp function edted

--- function.cpp
+++ function.cpp
@@ -6,13 +6,31 @@ int add(int a, int b) {
     return c;
 };
 
+void swap1(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+};
+
+void swap2(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+};
+
 int main() {
     int x, y, z;
     x = 10;
     y = 5;
-    z = add(x, y);
+    z = add(x, y); // call by value
 
     cout << z << endl;
+
+    swap1(&x, &y); // call by address
+    cout << "x is " << x << endl << "y is " << y << endl;
+
+    swap2(x, y); // call by Reference
+    cout << "x is " << x << endl << "y is " << y << endl;
 
     return 0;
 }
