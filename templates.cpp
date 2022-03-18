200f12aa6dbbe265cc4d619132fae31a8030c259
Toushif <toushif.haq@gmail.com>
2022-03-18 05:33:51 +0530

    cpp templates file added

---
+++ templates.cpp
@@ -0,0 +1,8 @@
+// A template is an outer skeleton of a class which can be re-used multiple times for different data types. Just like generics in TypeScript.
+
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    
+};
